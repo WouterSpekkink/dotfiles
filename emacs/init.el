0f8fb0d763892e9c8a4d1a31e30986db457b7438
Wouter Spekkink <wahsp@tutanota.com>
2022-09-10 01:13:04 +0200

    Updated emacs config

--- emacs/init.el
+++ emacs/init.el
@@ -20,6 +20,9 @@
 ;; Use-package
 (straight-use-package 'use-package)
 
+;; Starting buffer
+(setq initial-buffer-choice t)
+
 ;; Undo
 (use-package undo-fu
   :straight t)
@@ -58,13 +61,11 @@
   (setq evil-want-integration t)
   (evil-collection-init))
 
-(load "~/.config/emacs/keybinds.el")
-
 ;;;;;;;;;;;;;;
 ;; Org-mode ;;
 ;;;;;;;;;;;;;;
 (use-package org
-  :straight t
+  :straight t (:type built-in)
   :config
   (setq org-agenda-files (quote("~/org/")))
   (setq org-directory "~/org/")
@@ -72,7 +73,13 @@
   (setq org-refile-targets (quote ((nil :maxlevel . 9)
 				   (org-agenda-files :maxlevel . 9)
 				   ("~/org/org-roam/" :maxlevel . 9))))
-
+  (setq org-agenda-files (quote("~/org/"
+				"~/org/synced/"
+				"~/org/org-roam/"
+				"~/org/org-roam/daily/"
+				"~/org/org-roam/references/"
+				)))
+  
   (setq org-refile-use-outline-path t)
 
   (setq org-refile-allow-creating-parent-nodes (quote confirm))
@@ -89,14 +96,297 @@
    '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
 
   (use-package org-bullets
-    :straight t))
+    :straight t)
 
-(dolist (hook '(org-mode-hook))
-  (add-hook hook (lambda () (flyspell-mode 1)))
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  (dolist (hook '(org-mode-hook))
+    (add-hook hook (lambda () (flyspell-mode 1)))
+    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+    (add-hook 'org-mode-hook (lambda () (org-indent-mode 1)))
+    (add-hook 'org-mode-hook (lambda () (writegood-mode 1))))
+  (setq org-ellipsis " ▼ ")
+  (setq org-hide-emphasis-markers t)
+  (setq org-log-done 'time)
+
+  ;; org keyword related stuff
+  (setq org-todo-keywords
+	(quote ((sequence
+		 "TODO(t)"
+		 "PROJ(p)"
+		 "LOOP(r)"
+		 "STRT(s)"
+		 "IDEA(i)"
+		 "NEXT(n)"
+		 "|"
+		 "DONE(d)")
+		(sequence
+		 "WAIT(w@/!)"
+		 "HOLD(h@/!)"
+		 "|"
+		 "KILL(k@/!)")
+		(sequence
+		 "[ ](T)"
+		 "[-](S)"
+		 "[?](W)"
+		 "|"
+		 "[X](D)"
+		 ))))
+
+  (setq org-todo-keyword-faces
+	(quote (
+		("NEXT" +-lock-constant-face bold))))
+
+  (setq org-todo-state-tags-triggers
+	(quote (("KILL" ("KILL" . t))
+		("WAIT" ("WAIT" . t))
+		("HOLD" ("WAIT") ("HOLD" . t))
+		(done ("WAIT") ("HOLD"))
+		("TODO" ("WAIT") ("KILL") ("HOLD"))
+		("NEXT" ("WAIT") ("KILL") ("HOLD"))
+		("DONE" ("WAIT") ("KILL") ("HOLD")))))
+
+  ;; org capture related stuff
+  (setq org-capture-templates
+	(quote (("r" "respond" entry (file+headline "~/org/refile.org" "Emails")
+		 "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n")
+		("p" "project" entry (file+headline "~/org/refile.org" "Projects")
+		 "* PROJ %?\n%U\n%a\n")
+		("t" "todo" entry (file+headline "~/org/refile.org" "Tasks")
+		 "* TODO %?\nSCHEDULED: %t\n%U\n%a\n")
+		("i" "idea" entry (file+headline "~/org/refile.org" "Ideas")
+		 "* IDEA %?\n%U\n%a\n")
+		("e" "external" entry (file+headline "~/org/refile.org" "External")
+		 "* TODO %?\nSCHEDULED: %t\n%U\n%a\n %(progn (setq kk/delete-frame-after-capture 1) \"\")")
+		)))
+
+  ;; Caldav sync
+  (setq diary-location "~/.local/share/diary/")
+
+  (setq calendars
+	'(("outlook" . "http://localhost:1080/users/45995wsp@eur.nl/calendar/")
+	  ))
+
+  (defun getcal (url file)
+    "Download ics file and add it to file"
+    (let ((tmpfile (url-file-local-copy url)))
+      (icalendar-import-file tmpfile file)
+      (kill-buffer (car (last (split-string tmpfile "/"))))))
+
+  (defun getcals ()
+    "Load a set of ICS calendars into Emacs diary files"
+    (interactive)
+    (mapcar #'(lambda (x)
+		(let ((file (concat diary-location (car x)))
+		      (url (cdr x)))
+		  (message (concat "Loading " url " into " file))
+		  (find-file file)
+		  ;; (flush-lines "^[& ]") ;; if you import ical as non marking
+		  (erase-buffer) ;; to avoid duplicating events
+		  (getcal url file)
+		  ))
+	    calendars))
+
+  (setq org-agenda-include-diary t)
+  (setq diary-file "~/.local/share/diary/outlook")
+
+  ;; Kill capture frame
+  (defvar kk/delete-frame-after-capture 0 "Whether to delete the last frame after the current capture")
+
+  (defun kk/delete-frame-if-neccessary (&rest r)
+    (cond
+     ((= kk/delete-frame-after-capture 0) nil)
+     ((> kk/delete-frame-after-capture 1)
+      (setq kk/delete-frame-after-capture (- kk/delete-frame-after-capture 1)))
+     (t
+      (setq kk/delete-frame-after-capture 0)
+      (delete-frame))))
+
+  (advice-add 'org-capture-finalize :after 'kk/delete-frame-if-neccessary)
+  (advice-add 'org-capture-kill :after 'kk/delete-frame-if-neccessary)
+  (advice-add 'org-capture-refile :after 'kk/delete-frame-if-neccessary)
+
+  (setq org-refile-use-outline-path t)
+
+  (setq org-refile-allow-creating-parent-nodes (quote confirm))
+
+  (defun ws/verify-refile-target ()
+    "Eclude todo keywords with a done state"
+    (not (member (nth 2 (org-heading-components)) org-done-keywords)))
+
+  ;; Set up org-ref stuff
+  (use-package org-ref
+    :straight t
+    :custom
+    (org-ref-default-bibliography "/home/wouter/Tools/Zotero/bibtex/library.bib")
+    (org-ref-default-citation-link "citep")
+    (org-ref-insert-link-function 'org-ref-insert-link-hydra/body)
+    (org-ref-insert-cite-function 'org-ref-cite-insert-helm)
+    (org-ref-insert-label-function 'org-ref-insert-label-link)
+    (org-ref-insert-ref-function 'org-ref-insert-ref-link)
+    (org-ref-cite-onclick-function (lambda (_) (org-ref-citation-hydra/body))))
+
+   (defun my/org-ref-open-pdf-at-point ()
+    "Open the pdf for bibtex key under point if it exists."
+    (interactive)
+    (let* ((results (org-ref-get-bibtex-key-and-file))
+	   (key (car results))
+	   (pdf-file (funcall org-ref-get-pdf-filename-function key)))
+      (if (file-exists-p pdf-file)
+	  (find-file pdf-file)
+	(message "No PDF found for %s" key))))
+
+  (setq org-ref-completion-library 'org-ref-ivy-cite
+	org-export-latex-format-toc-function 'org-export-latex-no-toc
+	org-ref-get-pdf-filename-function
+	(lambda (key) (car (bibtex-completion-find-pdf key)))
+	org-ref-open-pdf-function 'my/org-ref-open-pdf-at-point
+	;; For pdf export engines
+	org-latex-pdf-process (list "latexmk -pdflatex='%latex -shell-escape -interaction nonstopmode' -pdf -bibtex -f -output-directory=%o %f")
+	org-ref-notes-function 'orb-edit-notes)
+
+  ;; Set up org-mode export stuff
+  (setq org-latex-to-mathml-convert-command
+        "java -jar %j -unicode -force -df %o %I"
+        org-latex-to-mathml-jar-file
+        "/home/wouter/Tools/math2web/mathtoweb.jar")
+
+  (add-to-list 'org-latex-classes
+               '("apa6"
+                 "\\documentclass{apa6}"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+  (add-to-list 'org-latex-classes
+               '("report"
+                 "\\documentclass{report}"
+                 ("\\chapter{%s}" . "\\chapter*{%s}")
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+
+  (add-to-list 'org-latex-classes
+               '("koma-article"
+                 "\\documentclass{scrartcl}"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+  (add-to-list 'org-latex-classes
+               '("memoir"
+                 "\\documentclass{memoir}"
+                 ("\\book{%s}" . "\\book*{%s}")
+                 ("\\part{%s}" . "\\part*{%s}")
+                 ("\\chapter{%s} .\\chapter*{%s}")
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+  (add-to-list 'org-latex-classes
+               '("paper"
+                 "\\documentclass{paper}"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+  (defun org-export-latex-no-toc (depth)
+    (when depth
+      (format "%% Org-mode is exporting headings to %s levels.\n"
+              depth)))
+
+  ;; org-noter stuff
+  (use-package org-noter
+    :straight t
+    :config
+    (setq
+     org-noter-notes-search-path "~/org/org-roam/references/"
+     org-noter-hide-other nil
+     org-noter-separate-notes-from-heading t
+     org-noter-always-create-frame t)
+    )
+  )
 
 (use-package org-roam
-  :straight t)
+  :straight t
+  :config
+  (setq org-roam-directory "~/org/org-roam/")
+  (add-to-list 'display-buffer-alist
+	       '("\\*org-roam\\*"
+		 (display-buffer-in-direction)
+		 (direction . right)
+		 (window-width . 0.33)
+		 (window-height . fit-window-to-buffer)))
+  (org-roam-db-autosync-mode)
+  ;(add-hook 'after-init-hook 'org-roam-mode)
+  
+  ;; org-roam-bibtex stuff
+  (use-package org-roam-bibtex
+    :straight t
+    :config
+    (org-roam-bibtex-mode))
+
+  (setq orb-preformat-keywords
+	'("citekey" "title" "url" "author-or-editor" "keywords" "file")
+	orb-process-file-keyword t
+	orb-file-field-extensions '("pdf"))
+
+  ;; Let's set up some org-roam capture templates
+  (setq org-roam-capture-templates
+	(quote (("d" "default" plain
+		 "%?"
+		 :target
+		 (file+head "%<%Y-%m-%d-%H%M%S>-${slug}.org"
+			    "#+title: ${title}\n")
+		 :unnarrowed t)
+		("r" "bibliography reference" plain
+		 (file "~/org/org-roam/templates/orb-capture")
+		 :target
+		 (file+head "references/${citekey}.org" "#+title: ${title}\n")))))
+
+  ;; And now we set necessary variables for org-roam-dailies
+  (setq org-roam-dailies-directory "daily/")
+  (setq org-roam-dailies-capture-templates
+	'(("d" "default" entry
+	   "* %?"
+	   :target
+	   (file+head "%<%Y-%m-%d>.org"
+		      "#+title: %<%Y-%m-%d>\n"))))
+
+  ;; Function to capture quotes from pdf
+  (defun org-roam-capture-pdf-active-region ()
+    (let* ((pdf-buf-name (plist-get org-capture-plist :original-buffer))
+	   (pdf-buf (get-buffer pdf-buf-name)))
+      (if (buffer-live-p pdf-buf)
+	  (with-current-buffer pdf-buf
+	    (car (pdf-view-active-region-text)))
+	(user-error "Buffer %S not alive" pdf-buf-name))))
+
+  ;; For org-roam-ui
+  (use-package websocket
+    :straight t)
+  (use-package org-roam-ui
+    :straight t
+    :config
+    (setq org-roam-ui-sync-theme t
+	  org-roam-ui-follow t
+	  org-roam-ui-update-on-save t))
+
+  ;; Workaround for org-roam minibuffer issues
+  (defun my/org-roam-node-read--to-candidate (node template)
+    "Return a minibuffer completion candidate given NODE.
+  TEMPLATE is the processed template used to format the entry."
+    (let ((candidate-main (org-roam-node--format-entry
+			   template
+			   node
+			   (1- (frame-width)))))
+      (cons (propertize candidate-main 'node node) node)))
+  (advice-add 'org-roam-node-read--to-candidate :override #'my/org-roam-node-read--to-candidate))
 
 ;;;;;;;;;;;
 ;; Email ;;
@@ -170,7 +460,10 @@
 		    (mu4e-update-interval             .       300))
 	    ))))
 ;; Email alert
-;(add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
+(use-package mu4e-alert
+  :straight t
+  :config
+  (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display))
 
 
 ;;;;;;;;;;;;;;
@@ -182,9 +475,54 @@
   :after flyspell
   :bind (:map flyspell-mode-map ("C-;" . flyspell-correct-wrapper)))
 
+
+(use-package langtool
+  :straight t
+  :config
+  (setq langtool-java-classpath
+	"/usr/share/languagetool:/usr/share/java/languagetool/*")
+  (setq langtool-default-language "en-GB"))
+
+(use-package writegood-mode
+  :straight t)
+
+;;;;;;;;;;;;;;;;
+;; Completion ;;
+;;;;;;;;;;;;;;;;
+
+(use-package company
+  :straight t
+  :config
+  (global-company-mode)
+  (setq company-show-numbers            t
+	company-minimum-prefix-length   1
+	company-idle-delay              0.5
+	company-backends
+	'((company-files          ; files & directory
+	   company-keywords       ; keywords
+	   company-capf           ; what is this?
+	   company-yasnippet)
+	  (company-abbrev company-dabbrev))))
+
+(use-package company-box
+  :straight t
+  :hook (company-mode . company-box-mode))
+
 ;;;;;;;;;;;;;;;
 ;; Utilities ;;
 ;;;;;;;;;;;;;;;
+(use-package general
+  :straight t
+  :config
+  (load "~/.config/emacs/keybinds.el")) 
+
+(setq delete-by-moving-to-trash t)
+
+;; Get rid of stupid sound
+(setq visible-bell 1)
+
+;; Auto-follow symbolic links
+(setq vc-follow-symlinks t)
 
 ;; Ivy
 (use-package ivy
@@ -225,6 +563,38 @@
 			     (name . "\*Apropos\*")
 			     (name . "\*info\*"))))))))		      
 
+;; helm-bibtex
+(use-package helm-bibtex
+  :straight t
+  :custom
+  (bibtex-completion-bibliography '("~/Tools/Zotero/bibtex/library.bib"))
+  (reftex-default-bibliography '("~/Tools/Zotero/bibtex/library.bib"))
+  (bibtex-completion-pdf-field "file")
+  :hook (Tex . (lambda () (define-key Tex-mode-map "\C-ch" 'helm-bibtex))))
+
+;; This is to use pdf-tools instead of doc-viewer
+(use-package pdf-tools
+  :straight t
+  :config
+  (pdf-tools-install)
+  (setq-default pdf-view-display-size 'fit-width)
+  :custom
+  (pdf-annot-activate-created-annotations t "automatically annotate highlights"))
+
+;; For deft
+(use-package deft
+  :straight t
+  :config
+  (setq deft-extensions '("org")
+        deft-directory "~/org/org-roam/"
+        deft-recursive t
+        deft-strip-summary-regexp ":PROPERTIES:\n\\(.+\n\\)+:END:\n"
+        deft-use-filename-as-title t))
+
+;; Treemacs
+(use-package treemacs
+  :straight t)
+
 ;;;;;;;;;;;;
 ;; Python ;;
 ;;;;;;;;;;;;
@@ -234,6 +604,63 @@
   :config
   (add-hook 'python-mode-hook 'anaconda-mode))
 
+;;;;;;;;;;;;;;;
+;; c and c++ ;;
+;;;;;;;;;;;;;;;
+
+;; lsp
+(use-package lsp-mode
+  :straight t
+  :hook ((c-mode . lsp)
+	 (c++-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp
+  :config
+  (setq lsp-file-watch-threshold 15000))
+
+(use-package lsp-ui
+  :straight t
+  :commands (lsp-ui-mode)
+  :config
+  (setq lsp-ui-doc-enable nil)
+  (setq lsp-ui-doc-delay 0.5))
+
+(use-package lsp-ivy
+  :straight t
+  :commands lsp-ivy-workspace-symbol)
+
+(use-package lsp-treemacs
+  :straight t
+  :commands lsp-treemacs-errors-list)
+
+(use-package ccls
+  :straight t
+  :config
+  (setq ccls-executable "/usr/bin/ccls")
+  (setq ccls-initialization-options
+	'(:index (:comments 2) :completion (:detailedLabel t))))
+
+(use-package modern-cpp-font-lock
+  :straight t)
+
+(use-package disaster
+  :straight t)
+
+;; flycheck
+(use-package flycheck
+  :straight t
+  :init (global-flycheck-mode)
+  :config
+  (setq flycheck-display-errors-function
+	#'flycheck-display-error-messages-unless-error-list)
+
+  (setq flycheck-indication-mode nil))
+
+(use-package flycheck-pos-tip
+  :straight t
+  :after flycheck
+  :config
+  (flycheck-pos-tip-mode))
 
 ;;;;;;;;;;;
 ;; Looks ;;
@@ -259,6 +686,13 @@
 (use-package all-the-icons
   :straight t
   :if (display-graphic-p))
+
+;; All the icons in dired
+(use-package all-the-icons-dired
+  :straight t
+  :config
+  (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
--- emacs/keybinds.el
+++ emacs/keybinds.el
@@ -1,24 +1,61 @@
-;; Evil-leader
-(use-package evil-leader
-  :straight t
-  :config
-  (global-evil-leader-mode)
-  (evil-leader/set-leader "<SPC>")
-  (evil-leader/set-key
-  "f" '("file" . (keymap))
-  "ff" '("find file" . find-file)
-  "fd" '("delete file" . delete-file)
+(general-define-key
+ :states '(normal visual)
+ :prefix "SPC"
+ :non-normal-prefix "M-SPC"
+ "f" '(:ignore t :wk "file")
+ "ff" '(find-file :wk "find file")
+ "fd" '(delete-file :wk "delete file")
 
-  "b" '("buffer" . (keymap))
-  "bb" '("switch buffer" . switch-to-buffer)
-  "bk" '("kill buffer" . kill-buffer)
-  "bl" '("buffer list" . ibuffer)
+ "b" '(:ignore t :wk "buffers")
+ "bb" '(switch-to-buffer :wk "switch buffer")
+ "bk" '(kill-buffer :wk "kill buffer")
+ "bl" '(ibuffer :wk "list buffers")
 
-  "w" '("window" . (keymap))
-  "ww" '("next window" . evil-window-next)
-  "wp" '("previous window" . evil-window-prev)
+ "w" '(:ignore t :wk "windows")
+ "ww" '(evil-window-next :wk "next window")
+ "wp" '(evil-window-prev :wk "previous window")
 
-  "o" '("open" . (keymap))
-  "om" '("email" . mu4e)
-  ))
+ "o" '(:ignore t :wk "open")
+ "om" '(mu4e :wk "email")
+ "ol" '(helm-bibtex :wk "literature")
+ "oa" '(org-agenda :wk "agenda")
+ "ot" '(treemacs :wk "treemacs")
 
+ "X" '(org-capture :wk "org capture")
+ "U" '(straight-pull-all :wk "update all packages"))
+
+;  (setq lsp-keymap-prefix "C-c l")
+ ; (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
+;   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+ ; (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references))
+
+(general-define-key
+ :prefix "SPC"
+ :states '(normal visual)
+ :non-normal-prefix "M-SPC"
+ :keymaps '(c++-mode-map c-mode-map)
+ "d" '(disaster :wk "disaster"))
+
+(general-define-key
+ :prefix "SPC"
+ :states '(normal visual)
+ :non-normal-prefix "M-SPC"
+ :keymaps 'org-mode-map
+ "e" '(org-export-dispatch :wk "export"))
+
+(eval-after-load "org-mode"
+  (general-define-key
+   :keymaps 'org-mode-map
+   "C-c [" nil
+   "C-c ]" 'org-ref-insert-link
+   "s-[" 'org-ref-insert-link-hydra/body))
+
+(eval-after-load "org-noter"
+  (general-define-key
+   :keymaps '(org-noter-insert-note org-noter-doc-mode-map)
+   "C-M-i" 'org-noter-insert-note
+   "C-M-p" 'org-noter-insert-precise-note
+   "C-M-k" 'org-noter-sync-prev-note
+   "C-M-j" 'org-noter-sync-next-note
+   "C-M-s" 'org-noter-create-skeleton
+   "C-M-q" 'org-noter-kill-session))
