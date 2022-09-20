e686829b76fd8689339a444454b072862fc67fc2
WouterSpekkink <wahsp@tutanota.com>
2022-09-20 22:36:50 +0200

    Changed theme and added org-modern

--- emacs/init.el
+++ emacs/init.el
@@ -76,7 +76,6 @@
 	 (org-mode . visual-line-mode))
   :config
   (setq org-directory "~/org/")
-  (setq org-ellipsis " ▼ ")
   (setq org-hide-emphasis-markers t)
   (setq org-log-done 'time)
   (setq org-agenda-window-setup "only window")
@@ -319,6 +318,40 @@
   :config
   (require 'evil-org-agenda)
   (evil-org-agenda-set-keys))
+
+(use-package org-modern
+  :straight t
+  :after org
+  :config
+  ;; Minimal UI
+  (package-initialize)
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+
+  (setq
+   ;; Edit settings
+   org-auto-align-tags nil
+   org-tags-column 0
+   org-catch-invisible-edits 'show-and-error
+   org-special-ctrl-a/e t
+   org-insert-heading-respect-content t
+
+   ;; Org styling, hide markup etc.
+   org-hide-emphasis-markers t
+   org-pretty-entities t
+   org-ellipsis "…"
+
+   ;; Agenda styling
+   org-agenda-tags-column 0
+   org-agenda-block-separator ?─
+   org-agenda-time-grid
+   '((daily today require-timed)
+     (800 1000 1200 1400 1600 1800 2000)
+     " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+   org-agenda-current-time-string
+   "⭠ now ─────────────────────────────────────────────────")
+  (global-org-modern-mode))
 
 ;;;;;;;;;;;
 ;; Email ;;
@@ -690,10 +723,10 @@
 ;;;;;;;;;;;
 
 ;; Set theme
-(use-package material-theme
+(use-package monokai-theme
   :straight t
   :config
-  (load-theme 'material t))
+  (load-theme 'monokai t))
 
 ;; Set font
 (add-to-list 'default-frame-alist '(font . "DejaVuSansMono NF Book 13"))
