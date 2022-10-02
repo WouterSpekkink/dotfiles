34da80ccc1a1b5e9d0ed93c442d347a27b45ddb6
WouterSpekkink <wahsp@tutanota.com>
2022-10-02 14:29:22 +0200

    Slight changes to sb-volume

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Autostart some programs
-pgrep pulseaudio || pulseaudio --start &
+pgrep pulseaudio || pulseaudio --start --daemonize &
 pgrep picom || picom -b --config ~/.config/picom/picom.conf &
 pgrep dwmblocks || dwmblocks &
 pgrep nitrogen || nitrogen --restore &
 pgrep wmname || wmname LG3D &
 pgrep emacs || emacs --daemon &
-#pgrep nm-applet || nm-applet &
 pgrep davmail || davmail &
 
 # Run startup scripts
