4cb303a35ed97898091ae5987344dfa6bc4bdd48
WouterSpekkink <wahsp@tutanota.com>
2023-03-31 15:56:03 +0200

    Fixed cleanup command and moved pulseaudio startup down (didn't solve my issues with it though)

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Autostart some programs
-pgrep pulseaudio || pulseaudio --start --daemonize &
 pgrep picom || picom -b --config ~/.config/picom/picom.conf &
 pgrep dwmblocks || dwmblocks &
 pgrep nitrogen || nitrogen --restore &
 pgrep wmname || wmname LG3D &
 pgrep emacs || emacs --daemon &
 pgrep davmail || davmail &
+pgrep pulseaudio || pulseaudio --start --daemonize &
 
 # Run startup scripts
 ~/.local/bin/checkaudio &
